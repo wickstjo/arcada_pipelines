df5b1665dd530be903f3c732f340c2b6c96d33a7
wickstjo <jfwick@gmail.com>
2024-11-06 02:05:28 +0200

    Got ray cluster up and running

---
+++ 06_kube/01_pods/04_ray/spam_pf.sh
@@ -0,0 +1,7 @@
+# DASHBOARD PORT        8265
+# CONNECTION PORT       10001
+
+while true; do
+    kubectl port-forward -n ray svc/raycluster-kuberay-head-svc --address 193.167.37.127 $1:$1
+    sleep 2;
+done
